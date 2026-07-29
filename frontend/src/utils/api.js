ea30bf217493f928c5dff1a93318d7e4d4dd8104
Sridharani <dharanisri915@gmail.com>
2026-07-30 02:19:19 +0530

    Configure production API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -58,7 +59,7 @@ api.interceptors.response.use(
       isRefreshing = true;
 
       try {
-        const res = await axios.post('http://localhost:5000/api/auth/refresh', {}, { withCredentials: true });
+        const res = await axios.post(`${API_URL}/auth/refresh`, {}, { withCredentials: true });
         if (res.data.success) {
           const newToken = res.data.data.token;
           localStorage.setItem('trippilot_token', newToken);
